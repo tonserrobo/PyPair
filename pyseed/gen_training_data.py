6c228d98c2a776c0d3cc71254d04e8195e522ce2
tonserrobo <45736383+tonserrobo@users.noreply.github.com>
2022-08-08 22:57:56 +0100

    class modification
    modification of IO, index and gen_training class. Cleaned and formated main.py

--- pyseed/gen_training_data.py
+++ pyseed/gen_training_data.py
@@ -1,11 +1,12 @@
 import pandas as pd 
 import os
 from encode_decode import Encode_decode
+import logging
+
+logging.basicConfig(level = logging.INFO)
 
 def generate_labeled_training_data(training_data, repetitions):
-        '''
-        Generate training data based on repeating the number of occurances of k-seed and its assocated org_location. 
-        '''
+        ''' Generate training data based on repeating the number of occurances of k-seed and its assocated org_location.  '''
         seed = []
         label = []
         
@@ -23,9 +24,7 @@ def generate_labeled_training_data(training_data, repetitions):
         return data
 
 def generate_training_data_full_read_map(training_data, repetitions):
-    '''
-    Generate training data based on repeating the number of occurances of k-seed and its assocated org_location. 
-    '''
+    ''' Generate training data based on repeating the number of occurances of k-seed and its assocated org_location.  '''
     seed = []
     
     for i in range(0,repetitions):
@@ -51,77 +50,76 @@ def generate_training_data_2(training_data, repetitions):
     for i in range(0, repetitions):
         data_temp = training_data[0:temp_range]
         data.append(data_temp)
-
     comp_data = pd.DataFrame(data)
     return comp_data
 
 def reverse_k_seed_string(training_data):
-    """
-    Method to reverse k-seed string and include within training set as additional feature
-    """
+    """ Method to reverse k-seed string and include within training set as additional feature """
     data = [] 
     for i in training_data['k-seed']:
         rev_seed = i[::-1]
         data.append(rev_seed)
-
     training_data['rev_k-seed'] = data
     del data
     return training_data
 
 def reverse_k_seed_string_full_map(training_data):
-    """
-    Method to reverse k-seed string and include within training set as additional feature
-    """
+    """Method to reverse k-seed string and include within training set as additional feature"""
     data = [] 
     for i in training_data['k-seed-extend']:
         rev_seed = i[::-1]
         data.append(rev_seed)
-
     training_data['rev_k-seed-extend'] = data
     del data
     return training_data
+
 class GenTraining():
-    """
-    Training data generation class and method returns a shuffled dataframe of k-seeds with position label, repeated x times.
-    """
-    def generate_training_data(self, reference_genome, training_iterations):
+    """ Training data generation class and method returns a shuffled dataframe of k-seeds with position label, repeated x times. """
+    def generate_training_data(self, training_iterations, seed_length, read_length):
         """
         Function call to GenTraining() class for training data generation. The action of generating training data will change the training
         flag to True, causing the model to retrain for the given training set. 
         """
-        # Read/Generate reference index file
+        logging.info('... Generating training data: Started')
         try:
             index = pd.read_csv("data_sets/indexed_reference.csv")
-        except FileNotFoundError:
-            print("indexed_reference.csv file not found") #not calling index gen from here
+        except Exception as e:
+            logging.error("Exception has occured generating training data from reference index", exc_info=True)
             #index = generate_index(reference_genome) 
 
         encoder_decoder = Encode_decode()
         
         # Generate and encode foward training data
+        logging.info('... ... Generating FWD labled training data: Started')
         training_set = generate_labeled_training_data(index, training_iterations)
         training_k_seeds = list(training_set['k-seed'])
-        training_set['k-seed'] = encoder_decoder.encode(training_k_seeds) # 2bit encoding
+        training_set['k-seed'] = encoder_decoder.encode(training_k_seeds)
+        logging.info('... ... Generating FWD labled training data: Compleate')
     
-        # Generate and encode reverse seed string
+        logging.info('... ... Generating REV labled training data: Started')
         training_set = reverse_k_seed_string(training_set)
         training_k_seeds_rev = list(training_set['rev_k-seed'])
         training_set['rev_k-seed'] = encoder_decoder.encode(training_k_seeds_rev)
+        logging.info('... ... Generating REV labled training data: Compleate')
 
-        #for full read mapping - fwd
-        training_set_full_map_fwd = generate_training_data_full_read_map(index, training_iterations)
-        training_k_seeds_extend_fwd = list(training_set_full_map_fwd['k-seed-extend'])
-        training_set['k-seed-extend'] = encoder_decoder.encode(training_k_seeds_extend_fwd) # 2bit encoding
+        if seed_length == read_length:
+            '''Check for full read mapping condition'''
+            logging.info('... ... Full read map FWD training data generation: Started')
+            training_set_full_map_fwd = generate_training_data_full_read_map(index, training_iterations)
+            training_k_seeds_extend_fwd = list(training_set_full_map_fwd['k-seed-extend'])
+            training_set['k-seed-extend'] = encoder_decoder.encode(training_k_seeds_extend_fwd) 
+            logging.info('... ... Full read map FWD training data generation: Compleate')
 
-        #for full read mapping - rev
-        training_set_full_map_rev = reverse_k_seed_string_full_map(training_set)
-        training_k_seeds_extend_rev = list(training_set_full_map_rev['rev_k-seed-extend'])
-        training_set['rev_k-seed-extend'] = encoder_decoder.encode(training_k_seeds_extend_rev)
+            logging.info('... ... Full read map REV training data generation: Started')
+            training_set_full_map_rev = reverse_k_seed_string_full_map(training_set)
+            training_k_seeds_extend_rev = list(training_set_full_map_rev['rev_k-seed-extend'])
+            training_set['rev_k-seed-extend'] = encoder_decoder.encode(training_k_seeds_extend_rev)
+            logging.info('... ... Full read map REV training data generation: Compleate')
         try:
             training_set.to_csv("data_sets/training_data.csv", index = False)
         except OSError:
             print("Cannot find data_sets directory, creating one...")
             os.makedirs('data_sets')
             training_set.to_csv("data_sets/training_data.csv", index = False)
-
+        logging.info('... Generating training data: Compleate')
         return training_set
--- pyseed/main.py
+++ pyseed/main.py
@@ -24,9 +24,10 @@ from numpy import std
 def main():
     """ for commonly used genome reference such as human, fish, mouse, rat - need something here to append the name of the ref used  """
     # config 
-    seed_length = 28 #defult seed length as used by BWA-MEM
-    training_iterations = 30 #looks optimum from sweeps
-    training_flag = False # used to trigger model retrain
+    seed_length = 28 
+    read_length = 100
+    training_iterations = 30 #30
+    training_flag = False 
 
     # imput arguments
     if len(sys.argv) > 1:
@@ -35,16 +36,10 @@ def main():
     else:
         reference_genome_file = 'reference_samples/s1.fasta'
         model_training_weights = "mouse"
-    
-    print(len(sys.argv))
-    print(str(sys.argv))
 
-    # # pharse reference sequence
     data = IO_processing()
     batch_size = len(reference_genome_file) #using full length as batch
-    sequence_id, sequence_string, sequence_length = data.pharse_reference(reference_genome_file, batch_size) #pharse reference sequence
-    
-    # # model weights lookup needs to happen here, or first just 
+    sequence_id, sequence_string, sequence_length = data.pharse_reference(reference_genome_file, batch_size)
     training_flag = data.weights_lookup(model_training_weights, training_flag)
 
     # model assignment
@@ -60,7 +55,13 @@ def main():
 
         # generate training data
         training_data_obj = GenTraining()
-        
+        training_data = training_data_obj.generate_training_data(training_iterations, seed_length, read_length) #will gen index if does not already exsist
+        df = training_data
+        x = df.loc[:,df.columns != 'position_label']
+        y = df.loc[:, 'position_label']
+        X_train, X_test, y_train, y_test = train_test_split(x,y, shuffle=True, test_size=0.2)
+        X_Val, X_test2, y_val, y_test2 = train_test_split(X_test, y_test, shuffle=True, test_size=0.2) #takes a further 20% as blind
+
         ## for training loop debugging and profiling
         import wandb
         wandb.init(
@@ -68,17 +69,6 @@ def main():
         notes="reference_index as training data",
         tags=["training data modification", "paper2"]
         )
-
-        training_data = training_data_obj.generate_training_data(reference_genome_file, training_iterations) #will gen index if does not already exsist
- 
-        df = training_data
-        x = df.loc[:,df.columns != 'position_label']
-        y = df.loc[:, 'position_label']
-
-        # Training/Test/Val split (80/20)
-        X_train, X_test, y_train, y_test = train_test_split(x,y, shuffle=True, test_size=0.2)
-        X_Val, X_test2, y_val, y_test2 = train_test_split(X_test, y_test, shuffle=True, test_size=0.2) #takes a further 20% as blind
-
         # model training
         model.fit(X_train, y_train)
         wandb.log({'Training Score': model.score(X_train, y_train), 'Test Score': model.score(X_test, y_test), 'Classification report': classification_report(y_test, model.predict(X_test)) })
@@ -89,6 +79,7 @@ def main():
         # Printing Accuracy on Training and Test sets
         print(f"Training Set Score : {model.score(X_train, y_train) * 100} %")
         print(f"Test Set Score : {model.score(X_test, y_test) * 100} %")
+        print(f"Val Set Score : {model.score(X_Val, y_val) * 100} %")
 
         # cross_validate also allows to specify metrics which you want to see
         for i, score in enumerate(cross_validate(model, X_test,y_test, cv=3)["test_score"]):
@@ -99,9 +90,17 @@ def main():
         predicted = model.predict(X_test)
         scores = cross_val_score(model, X_test, y_test, scoring='accuracy', cv=3, n_jobs=-1)
         print('Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
-        wandb.log({'K-Fold mean': mean(scores), 'k-Fold std': std(scores) })
+        wandb.log({'K-Fold mean': mean(scores), 
+                    'k-Fold std': std(scores) 
+                })
         precision, recall, fscore, support = f1_score(y_test, predicted)
-        wandb.log({'Precision': mean(precision), 'Recall': mean(recall), 'F1 Score':mean(fscore), 'Class Support min':min(support), 'Class Support Max': max(support), 'Class Support mean:':mean(support)})
+        wandb.log({'Precision': mean(precision), 
+                    'Recall': mean(recall), 
+                    'F1 Score':mean(fscore), 
+                    'Class Support min':min(support), 
+                    'Class Support Max': max(support), 
+                    'Class Support mean:':mean(support)
+                })
         wandb.finish()
 
         data.save_model_weights(model) #save model weights usign IOclass
